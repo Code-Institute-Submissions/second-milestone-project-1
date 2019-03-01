316b7340e378d372c94b8a24ae1d5672b3464501
Nadia <nbarreto@canadianbusinesscollege.com>
2019-03-01 14:39:25 +0000

    Number display for total crimes reported

---
+++ assets/scripts/graphs.js
@@ -0,0 +1,51 @@
+queue()
+    .defer(d3.csv,"assets/data/crimedata.csv")
+    .await(makeGraphs);
+    
+function makeGraphs(error, crimeData) {
+    //create a cross filter
+    var ndx=crossfilter(crimeData);
+	 crimeData.forEach(function(d) {
+        d.Total_sum = parseInt(d["count"]);
+    });
+    crimeData.forEach(function(d) {
+        if (d.province === "NL") {
+            d.province = "Newfoundland and Labrador"
+        }else if (d.province === "PE") {
+            d.province = "Prince Edward Island"
+        }else if (d.province === "NS") {
+            d.province = "Nova Scotia"
+        }else if (d.province === "NB") {
+            d.province = "New Brunswick"
+        }else if (d.province === "QC") {
+            d.province = "Quebec"
+        } else if (d.province === "ON") {
+            d.province = "Ontario"
+        } else if (d.province === "MB") {
+            d.province = "Manitoba"
+        }else if (d.province === "SK") {
+            d.province = "Saskatchewan"
+        }
+         else if (d.province === "AB") {
+            d.province = "Alberta"
+        }
+         else if (d.province === "BC") {
+            d.province = "British Columbia"
+        }
+    })
+    show_total_reported(ndx);
+    dc.renderAll();
+}
+
+function show_total_reported(ndx) {
+    var totalCrimes = ndx.groupAll().reduceSum(function(d) {
+        return d["Total_sum"];
+    });
+    dc.numberDisplay("#total-count")
+        .formatNumber(d3.format("d"))
+        .valueAccessor(function(d) {
+            return d;
+        })
+        .group(totalCrimes)
+        .formatNumber(d3.format(",.0f"));
+}
